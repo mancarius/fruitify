22699107734ad725b1276eb729ed386badafc7a4
mancarius <mattia.mancarella.dev@gmail.com>
2023-05-21 10:14:17 +0000

    fix app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'fruitify'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fruitify');
+    expect(app.title).toEqual('Fruitify');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fruitify app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Fruitify');
   });
 });

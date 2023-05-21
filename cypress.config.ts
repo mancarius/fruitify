0dcb83084a7def38cb940ba3964b27105cd16a18
mancarius <mattia.mancarella.dev@gmail.com>
2023-05-21 12:17:04 +0000

    fix cypress CI configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   video: false,
   screenshotOnRunFailure: true,
   e2e: {
-    baseUrl: "http://127.0.0.1:4200/",
+    baseUrl: "http://127.0.0.1:4200",
     supportFile: false,
     specPattern: 'cypress/integration/**/*.cy.js',
   }
